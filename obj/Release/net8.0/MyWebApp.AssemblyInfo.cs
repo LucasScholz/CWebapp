807d104631fa949caa44a91af7fc311e26b3a040
Lucas Scholz <lucas.scholz2103@gmail.com>
2025-08-04 15:43:40 +0200

    Buttons -> a und utf-8 bei error

--- obj/Release/net8.0/MyWebApp.AssemblyInfo.cs
+++ obj/Release/net8.0/MyWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31e867994b72bcad642a53d33764366d23c3cdb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f0d5516da81db2ea8955a9c8943d05d93042a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
